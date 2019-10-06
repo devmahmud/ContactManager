d7569acce9970b976690bfde6f2e3695bc9943fb
devmahmud <expelmahmud@gmail.com>
2019-10-06 23:50:50 +0600

    Updates

---
+++ precache-manifest.e4d229ceec1fc1f636e6791879b53f66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1dcc5866dc5bef3504f13885f3539e6",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "0eb8298edb9d550af4e9",
+    "url": "/ContactManager/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "0eb8298edb9d550af4e9",
+    "url": "/ContactManager/static/js/2.212b09f6.chunk.js"
+  },
+  {
+    "revision": "cdce2f5b73f4ec48ffe8",
+    "url": "/ContactManager/static/js/main.9cb83a90.chunk.js"
+  },
+  {
+    "revision": "27a0d1dac1e6fbec0361",
+    "url": "/ContactManager/static/js/runtime-main.21360655.js"
+  }
+]);
