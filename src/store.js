db82d79a6760478dfb35267d39aa539bb1e39f64
Mahmudul Alam <expelmahmud@gmail.com>
2019-10-07 10:31:13 +0600

    fixed redux dev tools issue

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import rootReducer from "./reducers";
@@ -10,10 +10,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+    applyMiddleware(...middleware)
 );
 
 export default store;
